c5522308e5e1bc8e17454079132395cb546068d9
Phuc La <phuc.la@asnet.com.vn>
2017-12-08 14:19:47 +0700

    Implement increment/decrement app

---
+++ practice-one/src/constants/constants.js
@@ -0,0 +1,8 @@
+export const Constant = {
+  INCREMENT: '+',
+  DECREMENT: '-',
+  BTN_INCREMENT: 'btn increment',
+  BTN_DECREMENT: 'btn decrement',
+  CONTAINER: 'container',
+  COUNTER: 'counter'
+}
