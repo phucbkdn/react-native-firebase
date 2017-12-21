977e6698beb62e330a13d624a56b5bf597a74c0e
Phuc La <phuc.la@asnet.com.vn>
2017-12-21 15:33:07 +0700

    #7 Create button delete

--- practice-2/src/App.js
+++ practice-2/src/App.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
-import './App.css';
+import IndexPage from './components/page/Index/Index';
+import Data from './components/common/Data';
+import ModalUpdate from './components/page/update/Modal';
+import { getProductByCategory, getCategory, deleteProduct } from './utils/HandleData';
 
 class App extends Component {
+  constructor(props) {
+    super();
+    const products = Data.products;
+    this.state = {
+      products: products
+    }
+  }
+
+  handleClickAdd = (product) => {
+
+  }
+
+  handleClickUpdate = (product) => {
+
+  }
+
+  /**
+   * Function using delete Product 
+   */
+  handleClickDelete = (id) => {
+    const products = Data.products;
+    const resultData = deleteProduct(products, id);
+    this.setState({
+      products: resultData
+    });
+  }
+
+  /**
+   * Function change Product list when select category
+   */
+  handleChangeItem = (id) => {
+    const products = Data.products;
+    const resultData = getProductByCategory(products, id);
+    this.setState({
+      products: resultData
+    });
+  }
+
+  getCategory = (categoryId) => {
+    const categorys = Data.category;
+    let category = getCategory(categorys, categoryId);
+    return category[0].name;
+  }
+
   render() {
+    const categorys = Data.category;
     return (
       <div className="App">
+        <IndexPage handleClickAdd={this.handleClickAdd}
+          categorys={categorys}
+          getCategory={this.getCategory}
+          changeItem={this.handleChangeItem}
+          products={this.state.products}
+          editItem={this.handleClickUpdate}
+          deleteItem={this.handleClickDelete} />
+        <ModalUpdate categorys={categorys} />
       </div>
     );
   }
