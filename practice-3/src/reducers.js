af24d64dd2238de9a5e238c9709da03cc3b60bd0
Phuc La <phuc.la@asnet.com.vn>
2018-01-09 10:36:26 +0700

    #18 Apply react router

--- practice-3/src/reducers.js
+++ practice-3/src/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import products from './pages/home/reducers/productReducer';
 import categorys from './common/reducers/dropdownReducer';
+import filterProduct from './pages/home/reducers/filterReducer';
 
 const productReducer = combineReducers({
   products: products,
-  categorys: categorys
+  categorys: categorys,
+  filterProduct:filterProduct
 });
 
 export default productReducer;
