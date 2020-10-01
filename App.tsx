5e8b8699041218f311948b8fcd9326aba0d6cb1e
Phuc La <phuc.la@asnet.com.vn>
2020-10-01 14:28:05 +0700

    Add google services json

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import * as Notifications from 'expo-notifications';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  useEffect(() => {
+    Notifications.addNotificationResponseReceivedListener(_handleNotificationResponse);
+  }, [])
+  // const _handleNotification = notification => {
+  //   this.setState({ notification: notification });
+  //   };
+
+  const _handleNotificationResponse = response => {
+    console.log(response);
+    };
   if (!isLoadingComplete) {
     return null;
   } else {
@@ -21,3 +39,4 @@ export default function App() {
     );
   }
 }
+
