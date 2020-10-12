daf3f44494362220f67de6cde934af2a3facf6c4
Phuc La <phuc.la@asnet.com.vn>
2020-10-12 17:53:35 +0700

    Handle send message

--- RN-rxjs/src/screens/Messages.tsx
+++ RN-rxjs/src/screens/Messages.tsx
@@ -1,44 +1,29 @@
 // Libs
-import { StackScreenProps } from '@react-navigation/stack'
 import React, { useState, useEffect, useRef } from 'react'
 import { Text, TouchableOpacity, View, TextInput, FlatList } from 'react-native'
 import moment from 'moment'
-import { FontAwesome } from '@expo/vector-icons';
-import { connect } from '../state/RXState'
-import {  counterActions } from '../reducers/categories'
-import firebaseApp,  { lazyMessages, addMessage } from '../services'
-import { list } from 'rxfire/database'
-import { map, filter } from 'rxjs/operators'
+import { FontAwesome } from '@expo/vector-icons'
+import { connect } from 'state/RXState'
+import { counterActions } from 'reducers/categories'
+import firebaseApp, { lazyMessages, addMessage } from 'services'
+import { filter } from 'rxjs/operators'
 import { authState } from 'rxfire/auth'
 import { useRoute } from '@react-navigation/native'
-import { combineLatest } from 'rxjs'
 
 // Helpers
-import { CategoryModel } from '../models'
-import { color } from '../themes'
-import { messagesStyles } from './styles/Messages.styles'
-import { RootStackParamList } from '../../types'
-import { User, MessageType } from '../models'
+import { color } from 'themes'
+import { messagesStyles } from 'screens/styles/Messages.styles'
+import { User, MessageType } from 'models'
 
 // Components
 import { Screen } from '../components/screen/screen'
 import Message from '../components/Message'
-import Users from './Users';
 
-interface TableOrderModel {
-  categories: Array<CategoryModel>,
-  navigation: StackScreenProps<RootStackParamList, 'NotFound'>,
-  increment: Function,
-  decrement: Function,
-  discountChange: Function,
-  discount: number,
-}
-
-const Messages = ({}: TableOrderModel) => {
+const Messages = () => {
   const { params } = useRoute()
   const [messages, setMessages] = useState<MessageType[]>([])
   const [message, setMessage] = useState<string>('')
-  const [user, setUser] = useState<User>({uid: '',email: ''})
+  const [user, setUser] = useState<User>({ uid: '', email: '' })
 
   const flatList = useRef<React.RefObject<FlatList<never>>>()
 
@@ -61,15 +46,17 @@ const Messages = ({}: TableOrderModel) => {
   // }, [])
 
   useEffect(() => {
-
-    const sub = lazyMessages('messages', params.name)
-      .subscribe((list: MessageType[]) => {
+    const sub = lazyMessages('messages', params.name).subscribe(
+      (list: MessageType[]) => {
         setMessages(list)
-      })
+      }
+    )
 
     const auth = firebaseApp.auth()
-    const loggedIn$ = authState(auth).pipe(filter(user => !!user));
-    loggedIn$.subscribe(user => { setUser(user) });
+    const loggedIn$ = authState(auth).pipe(filter((user) => !!user))
+    loggedIn$.subscribe((userData) => {
+      setUser(userData)
+    })
 
     return () => sub.unsubscribe()
   }, [])
@@ -96,30 +83,44 @@ const Messages = ({}: TableOrderModel) => {
         preset="fixed"
         backgroundColor={color.transparent}
       >
-      <Text style={messagesStyles.title}>{params.name}</Text>
-      <FlatList
-        ref={flatList}
-        onContentSizeChange={() => flatList.current.scrollToEnd({animated: true})}
-        onLayout={() => flatList.current.scrollToEnd({animated: true})}
-        data={messages}
-        renderItem={({ item }) =>
-        (<Message isPrimary={item.user === user.email} key={item.id} message={item.message} time={item.time} />)}
-        keyExtractor={item => item.id}
-      />
-        <View style={messagesStyles.textWrapper}>
-            <TextInput
-              style={messagesStyles.input}
-              onChangeText={handleChangeText}
-              keyboardType="ascii-capable"
-              value={message}
+        <Text style={messagesStyles.title}>{params.name}</Text>
+        <FlatList
+          ref={flatList}
+          onContentSizeChange={() =>
+            flatList.current.scrollToEnd({ animated: true })
+          }
+          onLayout={() => flatList.current.scrollToEnd({ animated: true })}
+          data={messages}
+          renderItem={({ item }) => (
+            <Message
+              isPrimary={item.user === user.email}
+              key={item.id}
+              message={item.message}
+              time={item.time}
             />
-            <TouchableOpacity onPress={sendMessage}>
-              <FontAwesome size={30} style={{ marginBottom: -3 }} name="send" />
-            </TouchableOpacity>
-          </View>
-        </Screen>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+        <View style={messagesStyles.textWrapper}>
+          <TextInput
+            style={messagesStyles.input}
+            onChangeText={handleChangeText}
+            keyboardType="ascii-capable"
+            value={message}
+          />
+          <TouchableOpacity onPress={sendMessage}>
+            <FontAwesome size={30} style={{ marginBottom: -3 }} name="send" />
+          </TouchableOpacity>
+        </View>
+      </Screen>
     </View>
-  );
+  )
 }
 
-export default connect(({ counter = {} }) => ({ categories: counter ? counter.categories : [], discount: counter.discount }), counterActions)(Messages)
+export default connect(
+  ({ counter = {} }) => ({
+    categories: counter ? counter.categories : [],
+    discount: counter.discount,
+  }),
+  counterActions
+)(Messages)
