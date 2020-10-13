50e515e1dd5a28d7fe00ca71b316b8d4bd00f5c4
Phuc La <phuc.la@asnet.com.vn>
2020-10-13 16:50:15 +0700

    Create unit test for user store

--- RN-rxjs/jest.config.js
+++ RN-rxjs/jest.config.js
@@ -3,28 +3,25 @@
 
 module.exports = {
   coverageDirectory: 'coverage',
-  preset: "react-native",
+  preset: 'react-native',
   setupFiles: [
-      "<rootDir>/node_modules/react-native/jest/setup.js",
-      "<rootDir>/src/test/setup.ts",
-      "<rootDir>/setupTest.ts",
-      "<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js"
-    ],
-  setupFilesAfterEnv: ["<rootDir>/src/test/timeout.ts",],
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/e2e"
+    '<rootDir>/node_modules/react-native/jest/setup.js',
+    '<rootDir>/src/test/setup.ts',
+    '<rootDir>/setupTest.ts',
+    '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
   ],
+  setupFilesAfterEnv: ['<rootDir>/src/test/timeout.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/e2e'],
   transformIgnorePatterns: [
-    "<rootDir>/node_modules/(?!(jest-)?react-native|react-native|@react-navigation|@storybook|@react-native-community)"
+    '<rootDir>/node_modules/(?!(jest-)?react-native|react-native|@react-navigation|@storybook|@react-native-community)',
   ],
   collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
+    'src/**/*.{js,jsx,ts,tsx}',
     '!<rootDir>/setupTests.ts',
     '!<rootDir>/coverage/**/*.js',
     '!<rootDir>/src/test/*.ts',
     '!<rootDir>/app/**/*.story.tsx',
   ],
-  timers: "fake",
-  testEnvironment: "jsdom"
-};
+  timers: 'fake',
+  testEnvironment: 'jsdom',
+}
--- RN-rxjs/src/test/firebase.ts
+++ RN-rxjs/src/test/firebase.ts
@@ -1,5 +1,21 @@
 jest.mock('firebase', () => {
-  const set = jest.fn()
+  const users = [
+    {
+      email: 'test-1@test.com',
+      id: 'IEa751YkWhXT0dKrKlNbK3e1AnA2',
+      last_changed: '2020-10-12T17:34:21+07:00',
+      state: 'offline',
+    },
+    {
+      email: 'test@test.com',
+      id: 'Kunp8W8bF6QjGDsXQ873bFjnutC3',
+      last_changed: '2020-10-07T17:36:43+07:00',
+      state: 'offline',
+    },
+  ]
+  const set = jest.fn(() => Promise.resolve(users))
+  const get = jest.fn(() => Promise.resolve(users))
+  const snapshot = { val: () => users }
   const onAuthStateChanged = jest.fn()
 
   const getRedirectResult = jest.fn(() => {
@@ -25,8 +41,19 @@ jest.mock('firebase', () => {
       ref: jest.fn(() => ({
         push: jest.fn(() => ({
           set,
+          get,
+          once: jest.fn(() => Promise.resolve(snapshot)),
         })),
       })),
+    })),
+    firestore: jest.fn(() => ({
+      collection: jest.fn(),
+      where: jest.fn(),
+      orderBy: jest.fn(),
+      add: jest.fn(),
+      get: jest.fn(),
+      onSnapshot: jest.fn(),
+      reset: jest.fn(),
     })),
     auth: jest.fn().mockReturnValue({
       onAuthStateChanged,
@@ -49,8 +76,19 @@ jest.mock('firebase', () => {
         ref: jest.fn(() => ({
           push: jest.fn(() => ({
             set,
+            get,
+            once: jest.fn(() => Promise.resolve(snapshot)),
           })),
         })),
+      })),
+      firestore: jest.fn(() => ({
+        collection: jest.fn(),
+        where: jest.fn(),
+        orderBy: jest.fn(),
+        add: jest.fn(),
+        get: jest.fn(),
+        onSnapshot: jest.fn(),
+        reset: jest.fn(),
       })),
     }),
   }
