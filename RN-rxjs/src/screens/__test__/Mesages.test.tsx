66d278845df0cc6adffd16e66b3d724a10dc802f
Phuc La <phuc.la@asnet.com.vn>
2020-10-14 17:46:40 +0700

    Update unit test for messages screen

--- RN-rxjs/src/screens/__test__/Mesages.test.tsx
+++ RN-rxjs/src/screens/__test__/Mesages.test.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Messages from '../Messages'
 import renderer from 'react-test-renderer'
-import { TextInput } from 'react-native'
+import { TextInput, TouchableOpacity } from 'react-native'
 import MockedNavigator from '../../test/mock-navigator'
 
 describe('Testing Messages screen', () => {
   const Component = () => <Messages />
   const wrapper = renderer.create(<MockedNavigator component={Component} />)
-  test('Render correct component', () => {
+  test('Render correct component', async () => {
     expect(wrapper).toMatchSnapshot()
   })
 
   test('Render send message', () => {
     const input = wrapper.root.findByType(TextInput)
-    input.props.onChangeText()
+    const button = wrapper.root.findByType(TouchableOpacity)
+    // Testing input value
+    const props = 'Value'
+    input.props.onChangeText(props)
+    expect(input.props.value).toEqual(props)
+
+    // Testing clear Input value when send message
+    button.props.onPress()
+    expect(input.props.value).toEqual('')
   })
 })
