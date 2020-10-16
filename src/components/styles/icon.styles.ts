c0554fef19e46e739725c5c8ac4d9cc73779fdfc
Phuc La <phuc.la@asnet.com.vn>
2020-10-16 14:59:30 +0700

    Create icon component

---
+++ src/components/styles/icon.styles.ts
@@ -0,0 +1,16 @@
+import { ViewStyle } from 'react-native'
+
+export const iconStyles = {
+  container: {
+    alignItems: 'center',
+    backgroundColor: 'black',
+    borderColor: 'transparent',
+    borderRadius: 10,
+    borderWidth: 1,
+    height: 34,
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 18,
+    width: 34,
+  } as ViewStyle,
+}
