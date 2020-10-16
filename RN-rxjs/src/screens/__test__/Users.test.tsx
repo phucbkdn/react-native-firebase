fc77e1b86b66bcf727ba453a8c60a9d48cf71acb
Phuc La <phuc.la@asnet.com.vn>
2020-10-16 15:05:16 +0700

    Create setting screen

--- RN-rxjs/src/screens/__test__/Users.test.tsx
+++ RN-rxjs/src/screens/__test__/Users.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { TouchableOpacity } from 'react-native'
 import MockedNavigator from '../../test/mock-navigator'
 import Users from '../Users'
 
@@ -9,10 +8,5 @@ describe('Testing Users screen', () => {
   const wrapper = renderer.create(<MockedNavigator component={Component} />)
   test('Render correct component', async () => {
     expect(wrapper).toMatchSnapshot()
-  })
-
-  test('Handle sign out action', async () => {
-    const button = wrapper.root.findByType(TouchableOpacity)
-    await button.props.onPress()
   })
 })
