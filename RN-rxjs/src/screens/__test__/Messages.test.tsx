73982fe2d597f09ddb90efe9ee2247fe59d90735
Phuc La <phuc.la@asnet.com.vn>
2020-10-21 14:49:49 +0700

    Update unit test for messages screen

--- RN-rxjs/src/screens/__test__/Messages.test.tsx
+++ RN-rxjs/src/screens/__test__/Messages.test.tsx
@@ -3,18 +3,43 @@ import Messages from '../Messages'
 import renderer from 'react-test-renderer'
 import { TextInput, TouchableOpacity } from 'react-native'
 import MockedNavigator from '../../test/mock-navigator'
-import { setupFirebaseUnitTest } from '../../test/setupFirebaseTest'
-
+import { setupFirebaseUnitTest, teardown } from '../../test/setupFirebaseTest'
+// jest.mock('../../services/firebaseAccess')
 const { adminDB } = setupFirebaseUnitTest()
+
+afterAll(async () => {
+  await teardown()
+})
 
 describe('Testing Messages screen', () => {
   const Component = () => <Messages />
   const wrapper = renderer.create(<MockedNavigator component={Component} />)
   test('Render correct component', async () => {
     await adminDB.collection('messages').add({
-      message: 'hello',
-      sendTo: 'test@gmail.com',
+      created: '2020-10-12T09:22:36.555Z',
+      message: 'Hello',
+      sendTo: 'abc1@test.com',
+      thread: 'abc@test.com-abc1@test.com',
+      time: '4:02',
+      user: 'abc@test.com',
     })
+    await adminDB.collection('messages').add({
+      created: '2020-10-12T09:22:36.555Z',
+      message: 'Hi',
+      sendTo: 'abc1@test.com',
+      thread: 'abc@test.com-abc1@test.com',
+      time: '4:02',
+      user: 'abc@test.com',
+    })
+    await adminDB.collection('messages').add({
+      created: '2020-10-12T09:22:36.555Z',
+      message: 'Test',
+      sendTo: 'abc1@test.com',
+      thread: 'abc@test.com-abc1@test.com',
+      time: '4:02',
+      user: 'abc@test.com',
+    })
+
     const render = renderer.create(<MockedNavigator component={Component} />)
     expect(render).toMatchSnapshot()
   })
