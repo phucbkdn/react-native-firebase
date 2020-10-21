9a9228769aeccdbb43df139f4c9d01e165a75615
Phuc La <phuc.la@asnet.com.vn>
2020-10-21 16:32:36 +0700

    Update unit test for users screen

--- RN-rxjs/src/screens/__test__/Users.test.tsx
+++ RN-rxjs/src/screens/__test__/Users.test.tsx
@@ -2,6 +2,24 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import MockedNavigator from '../../test/mock-navigator'
 import Users from '../Users'
+import { setupFirebaseUnitTest, teardown } from '../../test/setupFirebaseTest'
+
+const { adminDB } = setupFirebaseUnitTest()
+
+beforeEach(async () => {
+  await adminDB.collection('users').add({
+    state: 'online',
+    email: 'abc1@test.com',
+  })
+  await adminDB.collection('users').add({
+    state: 'online',
+    email: 'test@test.com',
+  })
+})
+
+afterEach(async () => {
+  await teardown()
+})
 
 describe('Testing Users screen', () => {
   const Component = () => <Users />
