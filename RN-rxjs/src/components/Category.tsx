1fbea02462af41cf932b74ec3c45559f9cfe3286
Phuc La <phuc.la@asnet.com.vn>
2020-10-27 18:49:30 +0700

    Optimized assets images

--- RN-rxjs/src/components/Category.tsx
+++ RN-rxjs/src/components/Category.tsx
@@ -1,37 +1,34 @@
 import React, { FC, memo } from 'react'
-import {
-  View,
-  TouchableOpacity,
-  Text,
-  ImageResizeMode,
-} from 'react-native'
+import { View, TouchableOpacity, Text } from 'react-native'
 import { categoryStyles } from './styles/category.styles'
 import { CategoryModel } from '../models'
 
-interface Props extends CategoryModel{
-  incrValue: Function,
-  decrValue: Function,
+interface Props extends CategoryModel {
+  incrValue: Function
+  decrValue: Function
 }
 
-export const Category: FC<Props> = memo(({ name, price, count, id, incrValue, decrValue }: Props) => (
-  <View style={categoryStyles.wrapper}>
-    <Text style={categoryStyles.textStyle}>{`${name} (${price})`}</Text>
-    <TouchableOpacity
-      onPress={() => decrValue(id)}
-      disabled={count <= 0}
-      style={categoryStyles.button}
-    >
-      <Text>-</Text>
-    </TouchableOpacity>
-    <Text style={categoryStyles.count}>{count}</Text>
-    <TouchableOpacity
-      onPress={() => incrValue(id)}
-      style={categoryStyles.button}
-    >
-      <Text>+</Text>
-    </TouchableOpacity>
-  </View>
-))
+export const Category: FC<Props> = memo(
+  ({ name, price, count, id, incrValue, decrValue }: Props) => (
+    <View style={categoryStyles.wrapper}>
+      <Text style={categoryStyles.textStyle}>{`${name} (${price})`}</Text>
+      <TouchableOpacity
+        onPress={() => decrValue(id)}
+        disabled={count <= 0}
+        style={categoryStyles.button}
+      >
+        <Text>-</Text>
+      </TouchableOpacity>
+      <Text style={categoryStyles.count}>{count}</Text>
+      <TouchableOpacity
+        onPress={() => incrValue(id)}
+        style={categoryStyles.button}
+      >
+        <Text>+</Text>
+      </TouchableOpacity>
+    </View>
+  )
+)
 
 Category.defaultProps = {
   name: '',
--- RN-rxjs/src/components/Discount.tsx
+++ RN-rxjs/src/components/Discount.tsx
@@ -1,35 +1,47 @@
 import React, { FC, memo } from 'react'
-import {
-  View,
-  Text,
-} from 'react-native'
+import { View, Text } from 'react-native'
 import currencyFormatter from 'currency-formatter'
 import { discountStyles } from './styles/discount.styles'
 import { connect } from '../state/RXState'
 import { CategoryModel } from '../models'
 
 interface TableOrderModel {
-  categories: Array<CategoryModel>,
-  discount: number,
+  categories: Array<CategoryModel>
+  discount: number
 }
 
-export const Discount: FC<TableOrderModel> = memo(({categories = [], discount}: TableOrderModel) => {
-  let price = categories.reduce((accumulator, current) => accumulator + (current.count * current.price), 0);
-  const discountPrice = price * discount / 100
-  price = price - discountPrice
+export const Discount: FC<TableOrderModel> = memo(
+  ({ categories = [], discount }: TableOrderModel) => {
+    let price = categories.reduce(
+      (accumulator, current) => accumulator + current.count * current.price,
+      0
+    )
+    const discountPrice = (price * discount) / 100
+    price = price - discountPrice
 
-  return (
-    <View style={discountStyles.container}>
-      <View style={discountStyles.priceWrapper}>
-        <Text style={discountStyles.title}>Discount:  </Text>
-        <Text style={discountStyles.title}>{currencyFormatter.format(discountPrice, { locale: 'VN' })}</Text>
+    return (
+      <View style={discountStyles.container}>
+        <View style={discountStyles.priceWrapper}>
+          <Text style={discountStyles.title}>Discount: </Text>
+          <Text style={discountStyles.title}>
+            {currencyFormatter.format(discountPrice, { locale: 'VN' })}
+          </Text>
+        </View>
+        <View style={discountStyles.priceWrapper}>
+          <Text style={discountStyles.title}>Total: </Text>
+          <Text style={discountStyles.title}>
+            {currencyFormatter.format(price, { locale: 'VN' })}
+          </Text>
+        </View>
       </View>
-      <View style={discountStyles.priceWrapper}>
-        <Text style={discountStyles.title}>Total:  </Text>
-        <Text style={discountStyles.title}>{currencyFormatter.format(price, { locale: 'VN' })}</Text>
-      </View>
-    </View>
-  )}
+    )
+  }
 )
 
-export default connect(({ counter = {} }) => ({ categories: counter ? counter.categories : [], discount: counter.discount }), [])(Discount)
+export default connect(
+  ({ counter = {} }) => ({
+    categories: counter ? counter.categories : [],
+    discount: counter.discount,
+  }),
+  []
+)(Discount)
