75fde3705e2a645161f02a359727d0361ab647af
Phuc La <phuc.la@asnet.com.vn>
2020-11-04 15:51:05 +0700

    Update navigation config support web view

--- RN-rxjs/src/navigation/index.tsx
+++ RN-rxjs/src/navigation/index.tsx
@@ -1,8 +1,13 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
+import * as Linking from 'expo-linking'
 import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
+  NavigationState,
+  PartialState,
+  NavigationContainerRef,
+  getStateFromPath,
 } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { map } from 'rxjs/operators'
@@ -28,10 +33,63 @@ import withObservableStream from '../streams'
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
 export default function Navigation() {
   const { theme } = useContext(AuthUserContext)
+  const getActiveRouteName = (
+    state: NavigationState | PartialState<NavigationState> | undefined
+  ): string => {
+    if (!state || typeof state.index !== 'number') {
+      return 'Unknown'
+    }
+
+    const route = state.routes[state.index]
+
+    if (route.state) {
+      return getActiveRouteName(route.state)
+    }
+
+    return route.name
+  }
+  const routeNameRef = useRef() as React.MutableRefObject<string | undefined>
+  const navigationRef = useRef() as React.RefObject<NavigationContainerRef>
+
+  const linkKing = {
+    prefixes: [Linking.makeUrl('/')],
+    config: {
+      screens: {
+        Users: 'users/',
+        Login: 'login',
+        Messages: 'messages',
+        NotFound: '*',
+      },
+    },
+    getStateFromPath,
+    getPathFromState(state, config) {
+      console.log(config, state)
+      if (!state || typeof state.index !== 'number') {
+        return 'NotFound'
+      }
+
+      const route = state.routes[state.index]
+
+      if (route.state) {
+        return getActiveRouteName(route.state)
+      }
+
+      return route.name
+    },
+  }
+
   return (
     <NavigationContainer
-      linking={LinkingConfiguration}
+      ref={navigationRef}
+      onReady={() =>
+        (routeNameRef.current = navigationRef.current?.getCurrentRoute()?.name)
+      }
+      linking={linkKing}
       theme={theme === 'dark' ? DarkTheme : DefaultTheme}
+      onStateChange={() => {
+        const currentRouteName = navigationRef.current?.getCurrentRoute()?.name
+        routeNameRef.current = currentRouteName
+      }}
     >
       <RootNavigator />
     </NavigationContainer>
